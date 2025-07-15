ecaaad7c483371ca8f7b052820da529f45f6ba0c
sankalpmtellur <sankalpmtellur282006@gmail.com>
2025-07-15 17:28:33 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 // main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
